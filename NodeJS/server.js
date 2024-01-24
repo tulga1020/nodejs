784356470b1999b506d04bd1b061fa16ea5e8a11
btgit <tulga1020@gmail.com>
2024-01-24 12:48:54 +0800

    sdfsdf

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,11 +1,34 @@
 const http = require("http");
+const { open } = require("node:fs/promises");
+
+const getWords = async () => {
+  const words = [];
+
+  const file = await open("./users.csv");
+  const text = await file.readLines();
+
+  for await (const line of text) {
+    words.push(line);
+  }
+  return words;
+};
+
 const port = 8000;
-const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader("Content-Type", "text/plain");
-  response.end("Hello World\n Hi ");
+const server = http.createServer(async (request, response) => {
+  try {
+    const words = await getWords();
+    const newWords = words.map((el) => {
+      return el.split(",");
+    });
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/plain");
+    response.end(`${newWords[0]} ,${newWords[1]}, hi`);
+  } catch (error) {
+    response.statusCode = 400;
+    console.log(error.message, `Aldaa garsan bna`);
+  }
 });
 
 server.listen(port, () => {
-  console.log(`Server runn ing at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
---
+++ NodeJS/writetext.js
@@ -0,0 +1,37 @@
+import { open } from "node:fs/promises";
+import express from "express";
+import fs from "fs";
+import mock from "./mock.json" assert { type: "json" };
+const app = express();
+const port = 8000;
+const ob = { email: "tulga1020@gmail.com", fullName: "Battulga.G" };
+app.get("/", (req, res) => {
+  
+  try {
+    res.status(200);
+    res.setHeader("Content-Type", "application/json");
+    res.send(if( {
+       // console.log("file chin ustchihsan ym bish uu? hugshuun");
+      } JSON.stringify(mock));
+  } catch (err) {
+    res.send("file aa usgatsan bn aa");
+  }
+  res.send("file ustsan ym bish uu");
+});
+app.delete("/", (req, res) => {
+  try {
+    fs.unlinkSync("mock.json");
+    res.status(200);
+    res.send("Delete File successfully.");
+  } catch (error) {
+    res.send("file not found");
+  } finally {
+    res.send("za yuch bolsongui");
+  }
+  res.status(200);
+  res.send("Ustgachihsan shuu haha");
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
